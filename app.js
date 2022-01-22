68d93738d478449e4e554ca7098ffbadf7c30813
pattWill <pattywilliams20@gmail.com>
2022-01-22 14:04:19 -0300

    agregando la ruta a los archivos

--- app.js
+++ app.js
@@ -3,8 +3,40 @@ const app = express();
 
 app.listen(3030, ()  => console.log('Levantando un servidor con Express'));
 
-app.get('/home', (req, res) => { 
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/index.html'));
+    ;
+ });
+ 
+ app.get('/babbage', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/berners-lee.html'));
+    ;
+ });
+ 
+ app.get('/clarke', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/clarke.html'));
+    ;
+ });
+
+ app.get('/hamilton', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/hamilton.html'));
+    ;
+ });
+
+
+ app.get('/hopper', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/hopper.html'));
+    ;
+ });
+
+ app.get('/lovelace', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/lovelace.html'));
+    ;
+ });
+ 
+
+ app.get('/turing', (req, res) => { 
+    res.sendFile(path.join(__dirname, './views/turing.html'));
     ;
  });
  
